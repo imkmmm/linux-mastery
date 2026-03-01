e5282f5b1cdefc2be89882f1ae7ed69cd45573e1
Imran <imranhakimazam@gmail.com>
2026-03-01 17:20:40 +0800

    Hotfix: added space to output string

--- check_logic.sh
+++ check_logic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Requirements: /shared_data directory must be set to chmod 745
-FILE="/shared_data/foundation.txt"
+FILE="$DATA_PATH/foundation.txt"
 
 if [ -f "$FILE" ]; then
-    echo "FOUND:Foundation File."
+    echo "FOUND: Foundation File."
 else
     echo "File missing. Creating now..."
     date > "$FILE"
